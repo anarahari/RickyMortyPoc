49a2472361f0f08d75205c7d3e707b0a13012ccc
anarahari <anarahari@griddynamics.com>
2024-11-13 18:24:20 +0530

    feat : Feature screen one, domain module setup

---
+++ domain/src/main/java/com/compose/domain/repository/CharacterRepository.kt
@@ -0,0 +1,10 @@
+package com.compose.domain.repository
+
+import androidx.paging.PagingData
+import com.common.graphql.GetCharactersQuery
+import kotlinx.coroutines.flow.Flow
+
+interface CharacterRepository {
+
+    suspend fun getCharacters() : Flow<PagingData<GetCharactersQuery.Result>>
+}
