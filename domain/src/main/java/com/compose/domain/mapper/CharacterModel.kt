6a78a467dc3abf5ed3df88116ae8b800c6299bff
anarahari <anarahari@griddynamics.com>
2024-11-22 17:15:40 +0530

    feat : Screen Two Domain
    - CharacterModel updated with episode and origin model fields

--- domain/src/main/java/com/compose/domain/mapper/CharacterModel.kt
+++ domain/src/main/java/com/compose/domain/mapper/CharacterModel.kt
@@ -6,5 +6,24 @@ data class CharacterModel(
     val image: String,
     val gender: String,
     val species: String,
-    val status: String
+    val status: String,
+    val created: String,
+    val origin: OriginModel = OriginModel(),
+    val episodes: List<EpisodeModel> = emptyList(),
+)
+
+data class OriginModel(
+    val id: String? = null,
+    val name: String? = null,
+    val type: String? = null,
+    val dimension: String? = null,
+    val created: String? = null,
+)
+
+data class EpisodeModel(
+    val id: String,
+    val name: String,
+    val airDate: String,
+    val episode: String,
+    val created: String,
 )
