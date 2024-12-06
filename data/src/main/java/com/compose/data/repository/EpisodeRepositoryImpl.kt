db056aadc03c104f9c70bbe76dd585e1a1b6c6fd
anarahari <anarahari@griddynamics.com>
2024-12-06 19:01:48 +0530

    feat : Feature screen three Data
    - Added Resource class to the episode respository to handle success and error in a proper way, so that error can also be propogated ahead to the use case and viewmodel.

--- data/src/main/java/com/compose/data/repository/EpisodeRepositoryImpl.kt
+++ data/src/main/java/com/compose/data/repository/EpisodeRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.compose.data.repository
 
 import com.apollographql.apollo.ApolloClient
 import com.common.graphql.GetEpisodeDetailsQuery
+import com.compose.common.R
+import com.compose.common.Resource
 import com.compose.data.datamapper.getEpisodeDetailsQueryToEpisodeModel
 import com.compose.domain.mapper.Episode
 import com.compose.domain.repository.EpisodeRepository
@@ -10,12 +12,18 @@ import javax.inject.Inject
 internal class EpisodeRepositoryImpl @Inject constructor(
     private val apolloClient: ApolloClient
 ) : EpisodeRepository {
-    override suspend fun getEpisodeDetails(id: String): Episode? {
+    override suspend fun getEpisodeDetails(id: String): Resource<Episode?> {
         return try {
             val response = apolloClient.query(GetEpisodeDetailsQuery(id)).execute()
-            getEpisodeDetailsQueryToEpisodeModel(response.data?.episode)
+            if (response.hasErrors()) {
+                Resource.Error(
+                    IllegalStateException(response.errors?.joinToString { it.message }).toString(),
+                    R.string.resp_failure
+                )
+            }
+            Resource.Success(data = getEpisodeDetailsQueryToEpisodeModel(response.data?.episode))
         } catch (ex: Exception) {
-            null
+            Resource.Error(ex.message.toString())
         }
     }
 }
